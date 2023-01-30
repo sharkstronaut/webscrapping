852d719faf4104a6b81fab531d9f0b2d7372d451
sharkstronaut <40474549+sharkstronaut@users.noreply.github.com>
2023-01-30 12:46:30 +0100

    refined search methods and added relevants to main

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ from nike_scrapper import *
 
 query = 'Air Jordan'
 
+
+
 nike_products = search_Nike(query)
 
-print(nike_products)
+print('Results of your query ', query, 'in the Nike website','\n', nike_products)
+print('\n')
 
-'''request = search_Stockx(query)
-print(request)
-'''
+
+print('First result found of your query ', query, 'in the Stockx website (raw data): ','\n', )
+first = search_firstProduct(query)
+print(first)
+
+print('\n')
+
+products = search_allProducts(query)
+relevant_data = get_productData(products)
+print('Total results of your query ', query, 'in the Stockx website:','\n', )
+for element in relevant_data:
+    print(element)
+    print('\n')
--- stockx.py
+++ stockx.py
@@ -2,6 +2,12 @@ import requests
 import re, json
 from bs4 import BeautifulSoup
 
+
+# Function to print json data in a legible way
+def printJSON(r):
+    parsed = json.loads(r)
+    print(json.dumps(parsed, indent = 4, sort_keys=False))
+
 '''
 (1) First try at a search method for sneakers. Returns captcha error.
 
@@ -35,7 +41,7 @@ def search(query):
     - Returns the first product listed that meets the requirements of the query.
 '''
 
-def search_Stockx(query):
+def search_firstProduct(query):
     url = f'https://stockx.com/api/browse?_search={query}'
 
     headers = {
@@ -57,3 +63,56 @@ def search_Stockx(query):
     html = requests.get(url=url, headers=headers)
     output = json.loads(html.text)
     return output['Products'][0]
+
+
+
+'''
+(3) Generalized method to find all the products that meet a certain criteria. Returns a dictionary.
+'''
+
+def search_allProducts(query):
+    url = f'https://stockx.com/api/browse?_search={query}'
+
+    headers = {
+        'accept': 'application/json',
+        'accept-encoding': 'utf-8',
+        'accept-language': 'en-GB,en;q=0.9',
+        'app-platform': 'Iron',
+        'referer': 'https://stockx.com/en-gb',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.62 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+
+    html = requests.get(url=url, headers=headers)
+    output = json.loads(html.text)
+    return output['Products']
+
+'''
+(4) Parse thru all products asked by the query and return relevant data, in this case:
+    - Cathegory
+    - Name
+    - Colorway
+    - Release date
+    - Retail price
+    - Highest bid for the product
+'''
+def get_productData(products):
+    sneaker_list = []
+    for e in products:
+        data = {
+            'category':e['category'],
+            'name':e['title'],
+            'colorway':e['colorway'],
+            'releaseDate':e['releaseDate'],
+            'retailPrice':e['retailPrice'],
+            'highestBid':e['market']['highestBid']
+        }
+        sneaker_list.append(data)
+
+    return sneaker_list
